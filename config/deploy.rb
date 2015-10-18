11e9dcb6b9a24dd7bdbd6358b2fcdecc7b61137d
wolfieorama <wachirawamunyi@gmail.com>
2015-10-18 13:24:51 +0300

    .... fixed error

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,6 @@
 server '188.166.6.35', port: 22, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:JohnSly/freshbox.git'
-set :branch,        :socialogin
 set :application,     'freshbox'
 set :user,            'deploy'
 set :puma_threads,    [4, 16]
